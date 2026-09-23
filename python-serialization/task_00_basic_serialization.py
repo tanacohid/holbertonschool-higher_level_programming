9f6c3bb04ceb64e6518f38ae4904c2370d587327
tanacohid <jeandbf5@gmail.com>
2026-09-23 11:20:04 +0200

    Add basic serialization and deserialization functions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic serialization and deserialization."""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file."""
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """Load a JSON file and return the deserialized dictionary."""
+    with open(filename, "r") as file:
+        return json.load(file)
